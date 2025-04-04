a6f73e4591642668591b87a4ecfbc644ec8dd241
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2025-04-04 18:45:36 +0200

    User y FileSystemEventNotifier

--- src/users/User.java
+++ src/users/User.java
@@ -1,4 +1,46 @@
 package users;
 
+import game.FileSystemEventNotifier;
+import java.nio.file.Path;
+import java.util.List;
+
 public class User {
+    private String name;
+    private String nick;
+    private FileSystemEventNotifier notifier;
+    private List<Path> pendingNotifications;
+
+    public void logout(){
+
+    }
+
+    public void dropout(){
+
+    }
+
+    public void operate(){
+
+    }
+
+    public String getName(){
+
+        return "";
+    }
+
+    public String getNick(){
+
+        return "";
+    }
+
+    public void update(Path file){
+
+    }
+
+    public FileSystemEventNotifier getNotifier() {
+        return notifier;
+    }
+
+    public User(String name, String nick){
+
+    }
 }
