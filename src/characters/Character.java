eac652cd2c03d27e8c4d6078eddbe504ed29142a
jaimeeg5 <j.esteban.2023@alumnos.urjc.es>
2025-04-10 19:48:54 +0200

    Creacion Clases

--- src/characters/Character.java
+++ src/characters/Character.java
@@ -1,4 +1,33 @@
 package characters;
 
-public class Character {
-}
+public abstract class Character {
+    private String name;
+    private SpecialAbility specialAbility;
+    private List<Weapon>  availableWeapons;
+    private List<Armor> availableArmors;
+    private Weapon[] Weapons= new Weapon[2];
+    private Armor activeArmor;
+    private List<Minion> minions;
+    private int gold;
+    private int health;
+    private int power;
+    private Modifier modifiers;
+
+    public void addMinion(Minion minion){
+
+    }
+
+    public void removeMinion(int index){
+
+    }
+
+    public void chooseArmor(Armor armor){
+
+    }
+
+    public abstract void attack();
+
+    public void setSpecialAbility(SpecialAbility specialAbility){
+
+    }
+    }
