968d32cf3a3bf3bd386f645a8816a10422d957f3
Fishmanaym <antoniomartincarrizosa@gmail.com>
2025-05-01 18:40:05 +0200

    operate player

--- src/game/Player.java
+++ src/game/Player.java
@@ -16,15 +16,65 @@ public class Player extends User {
 
     @Override
     public void operate(){
-
+        Menu menu = new Menu();
+        menu.setTitle("Elige una opción");
+        String[] menuOptions = {
+                "Gestionar personaje",
+                "Desafiar usuario",
+                "Consultar oro",
+                "Consultar ranking",
+                "Darse de baja"
+        };
+        menu.setOptions(menuOptions);
+        int choice;
+        do {
+            choice = menu.showMenu();
+            switch (choice) {
+                case 1:
+                    manageCharacter();
+                    break;
+                case 2:
+                    challengeUser();
+                    break;
+                case 3:
+                    checkGold();
+                    break;
+                case 4:
+                    checkRanking();
+                    break;
+                case 5:
+                    dropout();
+                    break;
+            }
+        } while (choice != 6 && choice != 5);
     }
+
+    private void manageCharacter() {
+        Menu menu = new Menu();
+        menu.setTitle("Gestión de personaje");
+        String[] menuOptions = {
+                "Ver datos de personaje",
+                "Gestionar equipamiento",
+                "Dar de baja personaje"
+        };
+    }
+
+    private void checkGold() {
+    }
+
+    private void checkRanking() {
+    }
+
+
+
+
 
     @Override
     public void update(Path file){
 
     }
 
-    public void challengeUser(User player){
+    public void challengeUser(){
 
     }
 
